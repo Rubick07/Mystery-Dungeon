75696dc30d7a590ae692c7a49b0cb969d00dc390
Rubick07 <evanjonatham07@gmail.com>
2026-04-24 15:01:03 +0700

    XCOM packages

---
+++ Assets/Scripts/UnitAnimator.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class UnitAnimator : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private Transform bulletProjectilePrefab;
+    [SerializeField] private Transform shootPointTransform;
+    [SerializeField] private Transform rifleTransform;
+    [SerializeField] private Transform swordTransform;
+
+    private void Start()
+    {
+        EquipRifle();
+    }
+
+    private void Awake()
+    {
+        if(TryGetComponent<MoveAction>(out MoveAction moveAction))
+        {
+            moveAction.OnStartMoving += MoveAction_OnStartMoving;
+            moveAction.OnStopMoving += MoveAction_OnStopMoving;
+        }
+        if (TryGetComponent<ShootAction>(out ShootAction shootAction))
+        {
+            shootAction.OnShoot += ShootAction_OnShoot;
+        }
+        if(TryGetComponent<SwordAction>(out SwordAction swordAction))
+        {
+            swordAction.OnSwordActionStarted += swordAction_OnSwordActionStarted;
+            swordAction.OnSwordActionCompleted+= swordAction_OnSwordActionCompleted;
+        }
+
+    }
+
+    private void swordAction_OnSwordActionCompleted(object sender, EventArgs e)
+    {
+        EquipRifle();
+    }
+
+    private void swordAction_OnSwordActionStarted(object sender, EventArgs e)
+    {
+        EquipSword();
+        animator.SetTrigger("SwordSlash");
+    }
+
+    private void MoveAction_OnStartMoving(object sender, EventArgs e)
+    {
+        animator.SetBool("IsWalking", true);
+    }
+    private void MoveAction_OnStopMoving(object sender, EventArgs e)
+    {
+        animator.SetBool("IsWalking", false);
+    }
+    private void ShootAction_OnShoot(object sender, ShootAction.OnShootEventArgs e)
+    {
+        animator.SetTrigger("Shoot");
+
+        Transform bulletProjectileTransform =
+            Instantiate(bulletProjectilePrefab,shootPointTransform.position , Quaternion.identity);
+
+        BulletProjectile bulletProjectile = bulletProjectileTransform.GetComponent<BulletProjectile>();
+
+        Vector3 targetUnitShootAtPosition = e.targetUnit.GetWorldPosition();
+
+        targetUnitShootAtPosition.y = shootPointTransform.position.y;
+
+        bulletProjectile.Setup(targetUnitShootAtPosition);
+
+
+    }
+
+    private void EquipSword()
+    {
+        swordTransform.gameObject.SetActive(true);
+        rifleTransform.gameObject.SetActive(false);
+    }
+
+    private void EquipRifle()
+    {
+        swordTransform.gameObject.SetActive(false);
+        rifleTransform.gameObject.SetActive(true);
+    }
+
+
+}
